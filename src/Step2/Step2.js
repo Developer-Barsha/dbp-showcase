acf516460ac772d49fb247d307c2d329e0c1f951
Developer-Barsha <lamiatabassumbarsha@gmail.com>
2022-07-23 20:59:37 +0600

    updated

--- src/Step2/Step2.js
+++ src/Step2/Step2.js
@@ -5,7 +5,7 @@ const Step2 = () => {
     return (
         <div className='step2'>
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Shopify_logo_2018.svg/2560px-Shopify_logo_2018.svg.png" alt="" />
-            <h3>Install our app on Shopify store :</h3>
+            <h3>Install our app on Shopify store:</h3>
         </div>
     );
 };
